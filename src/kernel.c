d99d57865ef9a80962288e5123df63ebc686b8ce
Jason Lingle <jasonlingle@yahoo.com>
2013-01-04 11:57:51 -0600

    Add documentation to kernel module.

--- src/kernel.c
+++ src/kernel.c
@@ -17,6 +17,13 @@
   along with Soliloquy.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+  TITLE: Input/Output Multiplexing and Task Control
+
+  OVERVIEW: The kernel module dispatches input, output, and timer events to
+    interested parties, in order to facilitate cooperative multi-tasking.
+*/
+
 #include "kernel.slc"
 
 #include <signal.h>
@@ -72,6 +79,33 @@ ATSTART(initialise_kernel, STATIC_INITIALISATION_PRIORITY) {
   signal(SIGPIPE, SIG_IGN);
 }
 
+/*
+  SYMBOL: $i_async_signal
+    When an asynchronous signal is being handled, this is set to the number of
+    that signal. Its value is undefined otherwise.
+
+  SYMBOL: $y_signal_is_synchronous
+    Set to true iff an asynchronous signal is being handled AND it is being
+    done so synchronously. If this is true, all functions are safe to call, as
+    the program is in a known-consistent state. If it is false, but
+    $y_is_handling_signal is true, there is very little you can do. In
+    particular, any action that could possibly interact with the heap is
+    off-limits and has undefined behaviour.
+
+  SYMBOL: $y_is_handling_signal
+    Set to true iff an asynchronous signal is currently being handled (even if
+    it is being handled synchronously).
+
+  SYMBOL: $i_sigchld_code $i_sigchld_pid $i_sigchld_status
+    When a SIGCHLD signal is being handled, set to the values of
+    siginfo_t::si_code, ::si_pid, and ::si_status for that signal (see
+    sigaction(2)).
+
+  SYMBOL: $f_sigchld
+    Called to handle incomming SIGCHLD signals. These are always handled
+    synchronously.
+*/
+
 static void handle_sigchld(int sigchld, siginfo_t* info, void* unknown) {
   $i_async_sygnal = sigchld;
   $y_signal_is_synchronous = true;
@@ -84,13 +118,41 @@ static void handle_sigchld(int sigchld, siginfo_t* info, void* unknown) {
   $y_is_handling_signal = false;
 }
 
+/*
+  SYMBOL: $f_save_the_world
+    Called when the program must be shut down abruptly. Hooks should be added
+    to save unmodified changes to recoverable locations; *nothing* should be
+    overwritten. *This may be called during an asynchronous signal handler.*
+    Hooks should try not to call non-async-safe operations, though some laxness
+    is tolerated here since this generally represents an already panicked
+    state. Hooks should also try to avoid assuming too much about the program
+    state, since this may be called as a result of a segmentation fault or
+    abort.
+
+  SYMBOL: $f_graceful_exit
+    Called after $f_save_the_world when the program is being terminated due to
+    an external signal (such as SIGTERM) rather than internal problems. It will
+    never be run in an asynchronous context.
+
+  SYMBOL: $f_die_gracelessly
+    Called after $f_save_the_world when the program is terminating due to
+    internal problems (such as receiving SIGSEGV). Hooks should try to clean up
+    _external_ concerns as best they can. Note that this is also called in the
+    case of SIGQUIT, since that signal is asynchronous. (The idea here is that
+    the user will only send SIGQUIT if SIGTERM fails to terminate the program,
+    which indicates an internal problem anyway, eg, an infinite loop.)
+*/
+
 static void handle_quit(int which) {
   $i_async_sygnal = which;
   $y_signal_is_synchronous = (which != SIGQUIT);
   $y_is_handling_signal = true;
 
   $f_save_the_world();
-  $f_graceful_exit();
+  if ($y_signal_is_synchronous)
+    $f_graceful_exit();
+  else
+    $f_die_gracelessly();
   exit(0);
 }
 
@@ -108,6 +170,23 @@ static void handle_fatal(int which) {
 }
 
 
+/*
+  SYMBOL: $c_Consumer
+    Encapsulates the state for an object which consumes data from an input file
+    descriptor. When read(2) will not block, the $m_read method will be called
+    on the Consumer; this method must not block.
+
+  SYMBOL: $i_Consumer_fd
+    The file descriptor backing the Consumer. It *must* be valid.
+
+  SYMBOL: $y_Consumer_has_priority
+    Within the $m_read method of a Consumer, indicates whether "priority" data
+    is available.
+
+  SYMBOL: $f_Consumer_destroy
+    Removes the Consumer from the consumer list.
+ */
+
 defun($h_Consumer) {
   $$lo_consumers = cons_o($o_Consumer, $$lo_consumers);
 }
@@ -115,6 +194,23 @@ defun($h_Consumer) {
 defun($h_Consumer_destroy) {
   $$lo_consumers = lrm_o($$lo_consumers, $o_Consumer);
 }
+
+/*
+  SYMBOL: $c_Producer
+    Encapsulates the state for an object which produces data to an output file
+    descriptor. When write(2) will not block, the $m_write method will be
+    called on the Producer; this method must not block.
+
+  SYMBOL: $i_Producer_fd
+    The file descriptor backing the Producer. It *must* be valid.
+
+  SYMBOL: $y_Producer_ready $y_Producer_hungup $y_Producer_error
+    When the $m_write method is called, these indicate the states of POLLOUT,
+    POLLHUP, and POLLERR for the Producer's file descriptor. (See poll(2).)
+
+  SYMBOL: $f_Producer_destroy
+    Removes the Producer from the producer list.
+ */
 
 defun($h_Producer) {
   $$lo_producers = cons_o($o_Producer, $$lo_producers);
@@ -128,11 +224,36 @@ advise_before($h_kernel_main) {
   $$y_keep_running = true;
 }
 
+/*
+  SYMBOL: $f_kernel_main
+    The main loop of the kernel. Typically called once for the whole program.
+*/
 defun($h_kernel_main) {
   while ($$y_keep_running)
     $f_kernel_cycle();
 }
 
+/*
+  SYMBOL: $f_kernel_cycle
+    Performs one task/io cycle; called by $h_kernel_main. Typically, you want
+    to hook onto $h_run_tasks() instead.
+
+  SYMBOL: $f_run_tasks
+    Called once per kernel cycle. Any periodic or cooperative tasks that need
+    to run should hook themselves into this point. When this is invoked,
+    $i_kernel_poll_duration_ms is 2**31-1 and $y_kernel_poll_infinite is
+    true. Hooks may alter these if they need to run on a temporal
+    basis. Conventionally, no hook should reset $y_kernel_poll_infinite to true
+    or increase $i_kernel_poll_duration_ms.
+
+  SYMBOL: $i_kernel_poll_duration_ms $y_kernel_poll_infinite
+    These two control the maximum length of the kernel cycle. If
+    $y_kernel_poll_infinite is true after the completion of $f_run_tasks, the
+    current kernel cycle will last until the next I/O event. If it is false,
+    the next kernel cycle will begin in approximately
+    $i_kernel_poll_duration_ms milliseconds or after the next I/O event,
+    whichever comes first.
+ */
 defun($h_kernel_cycle) {
   // Reset poll duration variables, then run one cycle for all tasks. If any
   // need to altern the poll duration, they can do so.
