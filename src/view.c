ab8a94b9a7ab078680b9fffd784aa827358293fe
Jason Lingle <jasonlingle@yahoo.com>
2013-01-10 13:13:07 -0700

    Basic implementation of View columnating and painting.

--- src/view.c
+++ src/view.c
@@ -44,17 +44,32 @@
   SYMBOL: $o_View_workspace
     The workspace bound to this View.
 
+  SYMBOL: $o_View_terminal
+    The terminal this View is bound to.
+
   SYMBOL: $lo_Terminal_views
     A listmap of all Views bound to the Terminal, keyed by Workspace.
 
   SYMBOL: $lo_Workspace_views
     A list of all Views bound to the Workspace.
+
+  SYMBOL: $i_View_cols
+    The number of text columns present in the view. This is not character
+    columns, but text columns; eg, a screen 180 characters wide might have two
+    text columns.
+
+  SYMBOL: $i_View_rows
+    The number of logical rows displayable within the View.
+
+  SYMBOL: $o_Terminal_current_view
+    The currently-active view of the Terminal.
  */
 defun($h_View) {
   $i_View_cut_on_screen = 0;
   $i_View_cut_in_workspace = $($($o_Workspace, $o_Workspace_backing),
                                $ao_Backing_lines)->len;
   $o_View_workspace = $o_Workspace;
+  $o_View_terminal = $o_Terminal;
   within_context($($o_View_workspace,$o_Workspace_backing),
                  ({
                    add_hook_obj(&$h_Backing_alter, HOOK_AFTER,
@@ -82,6 +97,10 @@ defun($h_View) {
                  }));
 
   $lo_Terminal_views = lmput_o($lo_Terminal_views, $o_View_workspace, $o_View);
+
+  $i_View_cols = $i_Terminal_cols / ($i_column_width + $i_line_meta_width);
+  if ($i_View_cols == 0) $i_View_cols = 1;
+  $i_View_rows = $i_View_cols * ($i_Terminal_rows - 1);
 }
 
 /*
@@ -113,3 +132,81 @@ defun($h_View_destroy) {
   $o_View_workspace = NULL;
   $lo_Terminal_views = lmdel_o($lo_Terminal_views, $o_View_workspace);
 }
+
+/*
+  SYMBOL: $f_View_redraw
+    Redraws the entire view, including pins and the echo area.
+ */
+defun($h_View_redraw) {
+  for (int $i_View_line_to_paint = 0;
+       $i_View_line_to_paint < $i_View_rows;
+       ++$i_View_line_to_paint)
+    $f_View_paint_line();
+  //TODO: pins
+
+  $f_View_draw_echo_area();
+}
+
+/*
+  SYMBOL: $f_View_backing_changed
+    Updates the view on the screen given the changes which occurred in the
+    backing, which must be the current context.
+ */
+defun($h_View_backing_changed) {
+  // Do nothing if not the current view of this View's terminal
+  if ($o_View != $($o_View_terminal,$o_Terminal_current_view))
+    return;
+
+  // If this was an append and we were at the end, move cut forward
+  if ($y_Backing_alteration_was_append &&
+      $i_View_cut_in_workspace == $i_Backing_alteration_begin)
+    $i_View_cut_in_workspace = $ao_Backing_lines->len;
+
+  for ($i_View_line_to_paint = $i_Backing_alteration_begin;
+       $i_View_line_to_paint < $i_View_cut_in_workspace;
+       ++$i_View_cut_in_workspace)
+    $F_View_paint_line(0,$o_View_terminal);
+}
+
+/*
+  SYMBOL: $f_View_paint_line
+    Paints the line indexed by $i_View_line_to_paint, assuming that the view is
+    the current view of its terminal, and that the line to paint is actually
+    visible.
+
+  SYMBOL: $i_View_line_to_paint
+    The line, as an index into the backing of the view's workspace, to paint in
+    a call to $f_View_paint_line.
+
+  SYMBOL: $i_column_width
+    The number of characters wide a column of text is.
+
+  SYMBOL: $i_line_meta_width
+    The width, in characters, of the line metadata area.
+ */
+defun($h_View_paint_line) {
+  int col = $i_View_line_to_paint / ($i_Terminal_rows-1);
+  int row = $i_View_line_to_paint % ($i_Terminal_rows-1);
+  col *= ($i_column_width + $i_line_meta_width);
+
+  qchar line[$i_column_width + $i_line_meta_width];
+  memset(line, 0, sizeof(line));
+
+  object oline =
+    $($($o_View_workspace,$o_Workspace_backing),
+      $ao_Backing_lines)->v[$i_View_line_to_paint];
+  $M_format_meta(0,oline,
+                 $p_format_dst = line, $i_format_size = $i_line_meta_width);
+  $M_format_main(0,oline,
+                 $p_format_dst = line + $i_line_meta_width,
+                 $i_format_size = $i_column_width);
+
+  within_context($o_View_terminal, ({
+        for (unsigned i = 0; i < sizeof(line)/sizeof(qchar); ++i)
+          $F_Terminal_putch(0,0, $i_x = col++, $i_y = row, $q_qch = line+i);
+        0;
+      }));
+}
+
+STATIC_INIT_TO($i_column_width, 80)
+STATIC_INIT_TO($i_line_meta_width, 4)
