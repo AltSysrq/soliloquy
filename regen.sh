10169402dc2d568c1a03ff0335a0d50e62d6ec63
Jason Lingle <jasonlingle@yahoo.com>
2013-01-12 20:11:39 +0000

    Don't use echo in regen.sh for compatiblity.

--- regen.sh
+++ regen.sh
@@ -4,16 +4,16 @@
 cp src/Makefile.am.head src/Makefile.am
 
 # List source files
-echo -n 'sol_SOURCES=' >>src/Makefile.am
+printf 'sol_SOURCES=' >>src/Makefile.am
 find src -name '*.c' -printf '%P ' >>src/Makefile.am
 echo >>src/Makefile.am
 # Compile silc files as needed
 find src -name '*.c' -printf '%P\n' | (while read name; do
     name=`echo $name | sed 's/.c$//'`
-    echo "$name.slc: $name.c silc dynar.plt list.plt"
-    echo "\\t@echo '  SILC   $name.slc'"
-    echo "\\t@./silc $name"
-    echo "$name.o: $name.slc"
+    printf "$name.slc: $name.c silc dynar.plt list.plt\n"
+    printf "\\t@echo '  SILC   $name.slc'\n"
+    printf "\\t@./silc $name\n"
+    printf "$name.o: $name.slc\n"
 done) >>src/Makefile.am
 
 autoreconf -i
