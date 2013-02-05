568b29dc1e148e018a91afc4c72351cd040d07a7
Jason Lingle <jasonlingle@yahoo.com>
2013-02-04 22:11:15 -0700

    Remove GNUisms of find usage in regen.sh.

--- regen.sh
+++ regen.sh
@@ -5,10 +5,10 @@ cp src/Makefile.am.head src/Makefile.am
 
 # List source files
 printf 'sol_SOURCES=' >>src/Makefile.am
-find src -name '*.c' -printf '%P ' >>src/Makefile.am
+find src -name '*.c' -exec printf '%s ' '{}' \; | sed 's:src/::g' >>src/Makefile.am
 echo >>src/Makefile.am
 # Compile silc files as needed
-find src -name '*.c' -printf '%P\n' | (while read name; do
+find src -name '*.c' -exec printf '%s\n' '{}' \; | sed 's:src/::g' | (while read name; do
     name=`echo $name | sed 's/.c$//'`
     printf "$name.slc: $name.c silc\$(EXEEXT) dynar.plt list.plt | classes\n"
     printf "\\t@echo '  SILC   $name.slc'\n"
